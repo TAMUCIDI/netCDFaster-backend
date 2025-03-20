d54be58e56f963417a2e22bec85235c3a79da4ef
laosong-2020 <songzl0606@gmail.com>
2025-03-20 15:05:53 -0500

    confidential

--- app/config.py
+++ app/config.py
@@ -1,17 +1,22 @@
+import os
+from dotenv import load_dotenv
+
 from pathlib import Path
 import redis
+
+load_dotenv(dotenv_path=".env")
 class Config:
     BASE_DIR = Path(__file__).resolve().parent.parent
     TMP_DIR = BASE_DIR / "tmp"
-    SECRET_KEY = '123456'
+    SECRET_KEY = os.getenv("SECRET_KEY")
     SESSION_TYPE = 'redis'
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
-    SESSION_COOKIE_SAMESITE = 'Lax'
+    SESSION_COOKIE_SAMESITE = os.getenv("SESSION_COOKIE_SAMESITE")
     SESSION_COOKIE_SECURE = False
     SESSION_REDIS = redis.Redis(
-        host='redis-15722.c244.us-east-1-2.ec2.redns.redis-cloud.com',
-        port=15722,
-        password='iE3DH0q7yjFAK4iLhCYiZHxuuWb0Z7tU'
+        host=os.getenv("REDIS_HOST"),
+        port=os.getenv("REDIS_PORT"),
+        password=os.getenv("REDIS_PASSWORD"),
     )
 
